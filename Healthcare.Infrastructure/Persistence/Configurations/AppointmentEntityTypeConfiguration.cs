0edef6051d12abb915d9bc34339b11ed5e65d706
NetNinjaEngineer <memo5260287@outlook.com>
2024-05-30 16:00:10 +0300

    Implemented appointment entity configuration

--- Healthcare.Infrastructure/Persistence/Configurations/AppointmentEntityTypeConfiguration.cs
+++ Healthcare.Infrastructure/Persistence/Configurations/AppointmentEntityTypeConfiguration.cs
@@ -5,7 +5,22 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 namespace Healthcare.Infrastructure.Persistence.Configurations;
 internal class AppointmentEntityTypeConfiguration : IEntityTypeConfiguration<Appointment>
 {
-    public void Configure(EntityTypeBuilder<Appointment> builder)
+    public void Configure(EntityTypeBuilder<Appointment> appointmentConfiguration)
     {
+        appointmentConfiguration.HasKey(x => x.Id);
+        appointmentConfiguration.Property(x => x.Id).ValueGeneratedOnAdd();
+
+        appointmentConfiguration.HasOne(x => x.Employee)
+            .WithMany(x => x.Appointments)
+            .HasForeignKey(x => x.EmployeeId)
+            .IsRequired();
+
+        appointmentConfiguration.HasOne(x => x.Patient)
+            .WithMany(x => x.Appointments)
+            .HasForeignKey(x => x.PatientId)
+            .IsRequired();
+
+        appointmentConfiguration.ToTable("Appointments");
+
     }
 }
